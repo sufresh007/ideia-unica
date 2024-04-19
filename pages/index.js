5ac184e21f41235e3610a859639965d5ae70d2e6
Miguel Alexandre Soares <miguelasoares54@gmail.com>
2024-04-19 09:13:23 -0300

    teste rapido

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,7 @@
-import { useState } from 'react';
-
 function Home(){
-    return (
-        <div>
-            <h4>quantas maricas tem me asssitindo? </h4>
-            <Contador />
-            <div>Teste</div>
-        </div>
-    )
-}
-
-function Contador(){
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button> 
+    return (<div>
+            <h1>Home</h1>
+            <a href="/Sobre">Acessar pagina sobre</a>
         </div>
     )
 }
---
+++ pages/sobre.js
@@ -0,0 +1,9 @@
+function Sobre(){
+    return (<div>
+            <h1>Sobre</h1>
+            <a href="/">Acessar pagina Home</a>
+        </div>
+    )
+}
+
+export default Sobre
