15217a79158bf6bdd99b74c29fafb072ebc632e3
Miguel Alexandre Soares <miguelasoares54@gmail.com>
2024-04-22 09:15:26 -0300

    mudanca botao

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
-import Link from 'next/link';
+import { AnnotationIcon, Button, Link } from "evergreen-ui"
+
 
 function Home(){
     return (<div>
             <h1>Home</h1>
 
+            <Button iconBefore = {AnnotationIcon} >botao</Button>
             <Link href="/sobre">
                 <a>Acessar pagina Sobre</a>   
             </Link>    
         </div>)
 }
+
+
 
 export default Home
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import { Link } from "evergreen-ui"
 
 function Sobre(){
     return (<div>
