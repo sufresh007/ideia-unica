055dde54ef8f70ce88eb74e2bc1ae6e296535d4d
Miguel Alexandre Soares <miguelasoares54@gmail.com>
2024-04-22 10:20:45 -0300

    api1

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -2,8 +2,8 @@ function tempo(request, response){
     const dynamicDate = new Date();
 
     response.json({
-        date: dynamicDate.toGMTString();
-    })
+        date: dynamicDate.toGMTString()
+    });
 }
 
 export default tempo;
